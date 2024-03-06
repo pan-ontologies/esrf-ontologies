de7830c55ecd682f8feaa04421db8c46503cb9fb
Giannis Koumoutsos <koumoutsosgian@yahoo.gr>
2024-03-06 16:10:08 +0100

    removed unused and black

--- src/esrf_ontology/techniques.py
+++ src/esrf_ontology/techniques.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from pydantic import BaseModel
 
 from . import panet_techniques
@@ -15,7 +14,7 @@ class TechniqueModel(BaseModel):
     @property
     def url(self) -> str:
         return _PANET_ID_TEMPLATE.format(self.panetid)
-    
+
     class Config:
         frozen = True
 
@@ -25,7 +24,6 @@ def get_technique(alias: str) -> TechniqueModel:
         return _ALIAS_TO_TECHNIQUE[alias]
     except KeyError:
         raise KeyError(f"'{alias}' is not a known technique alias") from None
-
 
 
 _ALIAS_TO_TECHNIQUE = panet_techniques.get_techniques_panet()
