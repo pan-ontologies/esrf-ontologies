c6bd2469425a4c9387abeccc506386ae77639c08
Marjolaine Bodin <bodin@esrf.fr>
2026-03-22 13:17:22 +0100

    #105 change versioned_iri

--- src/esrf_ontologies/technique/__init__.py
+++ src/esrf_ontologies/technique/__init__.py
@@ -47,6 +47,7 @@ def get_all_techniques() -> List[Technique]:
             names=tuple(technique["names"]),
             description=technique["description"],
             ontology_version=technique["ontology_version"],
+            versioned_iri=f'{technique["iri"].split("#")[0]}/{technique["ontology_version"]}/#{technique["iri"].split("#")[1]}',
         )
         for ontology_name in ["ESRFET"]
         for technique in _load_techniques(ontology_name)
